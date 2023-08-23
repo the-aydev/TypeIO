7e8c05c0f43ebce9f5259157e6ae94d8d216b50c
Ayomide <ayomidedans@gmail.com>
2023-08-23 10:25:40 +0100

    reactnode

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import Heading from "./components/Heading";
+import { Section } from "./components/Section";
+import Counter from "./components/Counter";
+import List from "./components/List";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState<number>(1);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Heading title="Hello TypeIO" />
+      <Section>
+        Section Node
+      </Section>
+      <Counter setCount={setCount}>Count is {count}</Counter>
+      <List items={["Coffee", "Tacos", "Code"]} render={(item: string) => <span className="gold">{item}</span>} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
